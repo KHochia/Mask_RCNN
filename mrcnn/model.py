4ecd05ceb0abb53b604723c0a0a6be236733352f
Hochia <56537599+KHochia@users.noreply.github.com>
2023-03-03 10:02:24 +0000

    multiprocessing off

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2367,7 +2367,8 @@ class MaskRCNN(object):
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
             workers=workers,
-            use_multiprocessing=workers > 1,
+            # use_multiprocessing=workers > 1,
+            use_multiprocessing=False
             verbose = 1,
         )
         self.epoch = max(self.epoch, epochs)
